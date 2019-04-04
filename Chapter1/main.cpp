849be6a84554e3a055a4a935c05cd37ffcbe64dd
Brendan A R Sechter <b-sechter@unext.jp>
2019-04-04 20:06:25 +0900

    Refactored code.

--- Chapter1/main.cpp
+++ Chapter1/main.cpp
@@ -11,12 +11,12 @@ void logSdlError(std::ostream &pOutputStream, const std::string pMessage) {
   pOutputStream << Constants::ApplicationName() << " " << pMessage << " Error: " << SDL_GetError() << std::endl;
 }
 
-int main(int argc, char** argv) {
+bool init(SDL_Window *&pWindow, SDL_Renderer *&pRenderer) {
   if (0 != SDL_Init(SDL_INIT_VIDEO)) {
     logSdlError(std::cout, "SDL_Init");
-    return EXIT_FAILURE;
+    return false;
   }
-  SDL_Window *window = SDL_CreateWindow(
+  pWindow = SDL_CreateWindow(
     Constants::WindowTitle(),
     Constants::WindowPositionX(),
     Constants::WindowPositionY(),
@@ -24,86 +24,109 @@ int main(int argc, char** argv) {
     Constants::WindowHeight(),
     SDL_WINDOW_SHOWN
   );
-  if (nullptr == window) {
+  if (nullptr == pWindow) {
     logSdlError(std::cout, "SDL_CreateWindow");
-    SDL_Quit();
-    return EXIT_FAILURE;
+    return false;
   }
-  SDL_Renderer *renderer = SDL_CreateRenderer(
-    window,
+  pRenderer = SDL_CreateRenderer(
+    pWindow,
     Constants::DefaultRendererWindow(),
     SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
   );
-  if (nullptr == renderer) {
+  if (nullptr == pRenderer) {
     logSdlError(std::cout, "SDL_CreateRenderer");
-    Utility::cleanup(window);
-    SDL_Quit();
-    return EXIT_FAILURE;
+    return false;
   }
+  return true;
+}
 
-  bool done = false;
-  int frame = 0;
-  do {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_QUIT:
-        case SDL_MOUSEBUTTONDOWN:
-        case SDL_KEYDOWN:
-          done = true;
-          break;
-        default:
-          break;
-      }
-    }
-    int framesPerCycle = 2.5 * Constants::FramesPerSecond();
-    int hue = frame / framesPerCycle % 6;
-    int intensity = 255 * abs(cos((float)frame / (2 * framesPerCycle) * M_PI));
-    int red, green, blue, alpha;
-    alpha = 255;
-    switch (hue) {
-      case 0:
-        red =  255;
-        green = 0;
-        blue = intensity;
-        break;
-      case 1:
-        red = 255;
-        green = intensity;
-        blue = 0;
-        break;
-      case 2:
-        red = intensity;
-        green = 255;
-        blue = 0;
-        break;
-      case 3:
-        red = 0;
-        green = 255;
-        blue = intensity;
-        break;
-      case 4:
-        red = 0;
-        green = intensity;
-        blue = 255;
-        break;
-      case 5:
-        red = intensity;
-        green = 0;
-        blue = 255;
-        break;
-    }
-    SDL_SetRenderDrawColor(renderer, red, green, blue, alpha);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    if (0 == frame % Constants::FramesPerSecond()) {
-      std::cout << Constants::ApplicationName() << " Frame: " << frame << std::endl;
-    }
-    frame++;
-    SDL_Delay(Constants::FrameWait());
-  } while (!done);
-  Utility::cleanup(renderer, window);
+void cleanup(SDL_Window *&pWindow, SDL_Renderer *&pRenderer) {
+  Utility::cleanup(pRenderer, pWindow);
   SDL_Quit();
-  return EXIT_SUCCESS;
+}
+
+void frameToColor(int pFrame, int &pRed, int &pGreen, int &pBlue, int &pAlpha) {
+  int framesPerCycle = 2.5 * Constants::FramesPerSecond();
+  int hue = pFrame / framesPerCycle % 6;
+  int intensity = 255 * abs(cos((float)pFrame / (2 * framesPerCycle) * M_PI));
+  pAlpha = 255;
+  switch (hue) {
+    case 0:
+      pRed =  255;
+      pGreen = 0;
+      pBlue = intensity;
+      break;
+    case 1:
+      pRed = 255;
+      pGreen = intensity;
+      pBlue = 0;
+      break;
+    case 2:
+      pRed = intensity;
+      pGreen = 255;
+      pBlue = 0;
+      break;
+    case 3:
+      pRed = 0;
+      pGreen = 255;
+      pBlue = intensity;
+      break;
+    case 4:
+      pRed = 0;
+      pGreen = intensity;
+      pBlue = 255;
+      break;
+    case 5:
+      pRed = intensity;
+      pGreen = 0;
+      pBlue = 255;
+      break;
+  }
+}
+
+void render(SDL_Renderer *pRenderer, int pFrame) {
+  int red, green, blue, alpha;
+  frameToColor(pFrame, red, green, blue, alpha);
+  SDL_SetRenderDrawColor(pRenderer, red, green, blue, alpha);
+  SDL_RenderClear(pRenderer);
+  SDL_RenderPresent(pRenderer);
+  if (0 == pFrame % Constants::FramesPerSecond()) {
+    std::cout << Constants::ApplicationName() << " Frame: " << pFrame << std::endl;
+  }
+}
+
+void update(int &pFrame) {
+  pFrame++;
+}
+
+void handleEvents(bool &pDone) {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+      case SDL_MOUSEBUTTONDOWN:
+      case SDL_KEYDOWN:
+        pDone = true;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+  bool error = false == init(window, renderer);
+  int frame = 0;
+  bool done = error;
+  while (!done) {
+    handleEvents(done);
+    update(frame);
+    render(renderer, frame);
+    SDL_Delay(Constants::FrameWait());
+  }
+  cleanup(window, renderer);
+  return error ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
